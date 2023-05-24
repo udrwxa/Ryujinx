9daa450425cd8df14c241ab02d3f959962b25909
Isaac Marovitz <isaacryu@icloud.com>
2023-05-23 22:17:54 -0400

    Start Metal Backend
    Revert build yml changes

--- src/Ryujinx/Program.cs
+++ src/Ryujinx/Program.cs
@@ -179,6 +179,10 @@ namespace Ryujinx.Ava
                 {
                     ConfigurationState.Instance.Graphics.GraphicsBackend.Value = GraphicsBackend.Vulkan;
                 }
+                else if (CommandLineState.OverrideGraphicsBackend.ToLower() == "metal")
+                {
+                    ConfigurationState.Instance.Graphics.GraphicsBackend.Value = GraphicsBackend.Metal;
+                }
             }
 
             // Check if docked mode was overriden.
