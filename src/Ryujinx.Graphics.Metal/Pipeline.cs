89d5e1ff869dac68e5a7bf772104e4467fe072bd
Samuliak <samuliak77@gmail.com>
2024-05-26 12:53:58 +0200

    account for 0 size

--- src/Ryujinx.Graphics.Metal/Pipeline.cs
+++ src/Ryujinx.Graphics.Metal/Pipeline.cs
@@ -183,7 +183,12 @@ namespace Ryujinx.Graphics.Metal
             var dest = new Texture(_device, this, textureInfo, drawable.Texture, 0, 0);
 
             // Regions
-            var srcRegion = new Extents2D(crop.Left, crop.Top, crop.Right, crop.Bottom);
+            var srcRegion = new Extents2D(
+                crop.Left,
+                crop.Top,
+                crop.Right == 0 ? texture.Width : crop.Right,
+                crop.Bottom == 0 ? texture.Height : crop.Bottom
+            );
             var dstRegion = new Extents2D(0, 0, (int)drawable.Texture.Width, (int)drawable.Texture.Height);
 
             _helperShader.BlitColor(tex, dest, srcRegion, dstRegion);
