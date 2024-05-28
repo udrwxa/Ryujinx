1b37245eb131a2c3bdbb3062ddec4dd70d26421b
Isaac Marovitz <isaacryu@icloud.com>
2024-05-28 00:50:10 -0400

    Rebase error

--- src/Ryujinx.Graphics.Shader/CodeGen/Msl/Instructions/IoMap.cs
+++ src/Ryujinx.Graphics.Shader/CodeGen/Msl/Instructions/IoMap.cs
@@ -26,7 +26,7 @@ namespace Ryujinx.Graphics.Shader.CodeGen.Msl.Instructions
                 IoVariable.FrontFacing => ("front_facing", AggregateType.Bool),
                 IoVariable.GlobalId => ("thread_position_in_grid", AggregateType.Vector3 | AggregateType.U32),
                 IoVariable.InstanceId => ("instance_id", AggregateType.S32),
-                IoVariable.InvocationId => ("INVOCATION_ID", AggregateType.S32),
+                IoVariable.InvocationId => ("thread_position_in_threadgroup", AggregateType.S32),
                 IoVariable.PointCoord => ("point_coord", AggregateType.Vector2),
                 IoVariable.PointSize => ("out.point_size", AggregateType.FP32),
                 IoVariable.Position => ("out.position", AggregateType.Vector4 | AggregateType.FP32),
